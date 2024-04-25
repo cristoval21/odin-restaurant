0c56fa6dab71e1fa39567e03b7c349909dcffbed
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-04-25 21:29:30 +0700

    wip: menu cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import Spaghetti from './assets/images/dish-1.jpg';
+
+const thumbnails = document.querySelectorAll(".card__thumbnail");
+thumbnails.forEach(thumbnail => {
+  thumbnail.src = Spaghetti;
+})
